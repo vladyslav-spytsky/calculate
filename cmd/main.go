00b25975042b9a543bbc50529fd58247d4991a03
spytskyi <v.spytskyi@gmail.com>
2022-04-08 09:27:49 +0300

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,23 @@
 package main
 
 import (
-	"calculate/conf"
-	"calculate/src/domain/infrastructure/delivery"
+	delivery2 "calculate/src/infrastructure/delivery"
 	"calculate/src/registry"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	// Init configs
-	conf.InitConfig()
-
 	// New router
 	router := httprouter.New()
 
 	r := registry.NewRegistry()
 
 	// Init router
-	router = delivery.NewRouter(router, r.NewAPIController())
+	router = delivery2.NewRouter(router, r.NewAPIController())
 
-	err := http.ListenAndServe(viper.GetString("http.port"), router)
+	err := http.ListenAndServe(":8989", router)
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -4,5 +4,4 @@ go 1.17
 
 require (
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/spf13/viper v1.10.1
 )
